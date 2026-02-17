af5d8b255f539e508d5dc3e859d2157a58cd2313
sakshisinha15 <sakshisinha2854@gmail.com>
2026-02-17 22:52:11 +0530

    Java Code (PEP Classes)

---
+++ JAVA/primenum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class primenum {
+    public static boolean prime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (prime(n)) {
+            System.out.println("Prime number");
+        } else {
+            System.out.println("Not a prime number");
+        }
+    }
+}
